94babf0bdab5a0656b29b3ca23fb972c993ed197
Dovydas-Kr <dovydas.kr@gmail.com>
2014-11-06 18:48:07 +0000

    Added Euler 1 and 2 Functions into app.py

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import os, sys
 app = Flask(__name__)
 
 @app.route("/")
@@ -37,3 +38,25 @@ def list_files():
      for file in dirs:
 	files = str(files + file + ", \n")
      return files
+ 
+#Euler 1 and 2 Lab7 functions:
+@app.route("/euler1")
+def euler1():
+     sum1 = 0
+     for i in range (1, 1000):
+	if i%3 or i%5:
+	   sum1 = sum1 + i
+     return str(sum1)
+
+@app.route("/euler2")
+def euler2():
+     term1 = 1
+     term2 = 2
+     sum1 = 0
+
+     for i in range (0, 4000000):
+	sum1 = term1 + term2
+	term1 = term2
+	term2 = sum1
+	answer = "Fibinacci up to 4000000 is " + str(sum1)
+     return answer
