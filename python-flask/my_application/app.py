7ffe5991659f93907d209cc4f4395d29e5b43874
Dovydas-Kr <dovydas.kr@gmail.com>
2014-11-06 15:55:58 +0000

    Added program to get list of files from a directory and return them

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
   
-  
+# 2 programs for lab 5
 @app.route("/intro")
 def introduction():
     return "Hello, I am Smurf!!"
   
-  
+
 @app.route("/sumIs")
 def sumIs():
     a = 2
@@ -27,3 +27,13 @@ def upload_file():
     return '',201
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+#Code to get the list of files from a directory and return it.
+@app.route("/files")
+def list_files():
+     path = "./uploads/"
+     dirs = os.listdir(path)
+     files = ""
+     for file in dirs:
+	files = str(files + file + ", \n")
+     return files
